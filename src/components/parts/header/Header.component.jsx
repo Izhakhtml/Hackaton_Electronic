14bfee2c5232191a8020272efb16a8be2465f201
avi-admaso <aviadmaso@gmail.com>
2022-02-10 04:42:50 +0200

    edit header and footer

--- src/components/parts/header/Header.component.jsx
+++ src/components/parts/header/Header.component.jsx
@@ -1,15 +1,26 @@
 import { Link } from "react-router-dom";
-const Header = ()=>{
-    return(
-        <header className="header">
-              <nav className="nav">
-                   <Link to="/"><li>Home</li></Link> 
-                   <Link to="/Computer"><li>Computer</li></Link> 
-                   <Link to="/Phone"><li>Phone</li></Link> 
-                   <Link to="/Tv"><li>Tv</li></Link>                    
-                   <Link to="/Refrigerator"><li>Refrigerator</li></Link> 
-              </nav>
-        </header>
-    )    
-    }
-    export default Header;
+const Header = () => {
+  return (
+    <header className="header">
+      <div id="theNav">
+        <nav className="nav">
+          <a class="active" href="index.html">
+            Home
+          </a>
+
+          <a href="/Computer">Computer</a>
+          <a href="/Phone">Phone</a>
+          <a href="/Tv">Tv</a>
+          <a href="/Refrigerator">Refrigerator</a>
+          <a href="/User-account">
+            <img
+              id="logInIcon"
+              src="https://cdn-icons-png.flaticon.com/512/456/456212.png"
+            />
+          </a>
+        </nav>
+      </div>
+    </header>
+  );
+};
+export default Header;
